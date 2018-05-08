75e3f4ca15cf36c07c59394c56ff61cb79942d80
NicoBrunello <nicolo.brunello@mail.polimi.it>
2018-05-08 14:31:33 +0200

    Added test class for RMIClient and RMIServer

---
+++ src/test/java/it/polimi/ingsw/Comunication/RMIClientTest.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.Comunication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RMIClientTest {
+
+    @Test
+    void connect() {
+    }
+
+    @Test
+    void pushData() {
+    }
+
+    @Test
+    void updatePlayersInfo() {
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/Comunication/RMIServerTest.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.Comunication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RMIServerTest {
+
+    @Test
+    void startGame() {
+    }
+
+    @Test
+    void acceptConnection() {
+    }
+
+    @Test
+    void notifyClients() {
+    }
+}
