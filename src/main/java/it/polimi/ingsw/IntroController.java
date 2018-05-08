67258bbc2ee960fe6c36d877749c8b0f960154a8
8549 <15671077+8549@users.noreply.github.com>
2018-05-08 21:16:55 +0200

    Add intro GUI (connection type, server details)

---
+++ src/main/java/it/polimi/ingsw/IntroController.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+
+public class IntroController {
+
+    @FXML
+    private ToggleGroup connectionType;
+
+}
---
+++ src/main/java/it/polimi/ingsw/IntroLoader.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class IntroLoader extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        URL url = getClass().getClassLoader().getResource("views/intro.fxml");
+        FXMLLoader loader = new FXMLLoader(url);
+        Parent root = loader.load();
+        primaryStage.setTitle("Sagrada - Connection");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
