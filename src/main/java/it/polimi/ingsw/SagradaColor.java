876a553f7a8fdf5ee2c6f5ea949e552343556eff
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2018-05-16 22:30:14 +0200

    Changed escape in sagrada color

--- src/main/java/it/polimi/ingsw/SagradaColor.java
+++ src/main/java/it/polimi/ingsw/SagradaColor.java
@@ -3,17 +3,17 @@ package it.polimi.ingsw;
 import javafx.scene.paint.Color;
 
 public enum SagradaColor {
-    BLUE("#2dbbc8", "\u001B[34m"),
-    GREEN("#03ab6c", "\u001B[32m"),
-    PURPLE("#a54198", "\u001B[35m"),
-    RED("#dc2327", "\u001B[31m"),
-    YELLOW("#f3de0c", "\u001B[33m");
+    BLUE("#2dbbc8", 34),
+    GREEN("#03ab6c", 32),
+    PURPLE("#a54198", 35),
+    RED("#dc2327", 31),
+    YELLOW("#f3de0c", 33);
 
     private Color color;
-    private String escape;
+    private int escape;
     public static final String RESET = "\u001B[0m";
 
-    SagradaColor(String hex, String escape) {
+    SagradaColor(String hex, int escape) {
         this.color = Color.web(hex);
         this.escape = escape;
     }
@@ -22,13 +22,17 @@ public enum SagradaColor {
         return color;
     }
 
-    public String getEscape() {
+    public int getEscape() {
         return escape;
+    }
+
+    public String escapeString() {
+        return "\u001B[" + escape + "m";
     }
 
     public static void main(String[] args) {
         for (SagradaColor c : SagradaColor.values()) {
-            System.out.println(c.getEscape() + c + SagradaColor.RESET);
+            System.out.println(c.escapeString() + c + SagradaColor.RESET);
         }
     }
 
