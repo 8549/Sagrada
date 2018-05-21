2bfd5b419dd555c14e887563a0f19a6ae909e1c6
8549 <15671077+8549@users.noreply.github.com>
2018-05-21 11:55:27 +0200

    Add cli parsing

--- src/main/java/it/polimi/ingsw/network/runServer.java
+++ src/main/java/it/polimi/ingsw/network/runServer.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.network;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
 import java.rmi.Naming;
 
 public class runServer {
@@ -8,12 +11,18 @@ public class runServer {
     public static void main(String[] args) {
 
         try {
+
+            OptionParser parser = new OptionParser();
+            parser.accepts("p").withRequiredArg().ofType(Integer.class).defaultsTo(DEFAULT_PORT);
+            OptionSet set = parser.parse(args);
+
+            int port = (int) set.valueOf("p");
             //System.setSecurityManager(new RMISecurityManager());
-            java.rmi.registry.LocateRegistry.createRegistry(DEFAULT_PORT);
+            java.rmi.registry.LocateRegistry.createRegistry(port);
 
             ServerInterface server = new RMIServer();
-            Naming.rebind("rmi://127.0.0.1:" + DEFAULT_PORT + "/sagrada", server);
-            System.out.println("[System] Server is ready.");
+            Naming.rebind("rmi://127.0.0.1:" + port + "/sagrada", server);
+            System.out.println("[System] Server is ready on port " + port);
         }catch (Exception e) {
             System.out.println("Server failed: " + e);
         }
