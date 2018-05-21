6ac42bb03a5987f119cccca87d841fe2974e9a48
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2018-05-21 17:47:18 +0200

    Added Constraint Controller class with its tests

---
+++ src/main/java/it/polimi/ingsw/ConstraintController.java
@@ -0,0 +1,116 @@
+package it.polimi.ingsw;
+
+public class ConstraintController {
+
+    /**
+     * Check if it's possible to place the first die at the given coordinates, they must be on the edge of the grid
+     *
+     * @param row
+     * @param column
+     * @return true if the coordinates are on the edge of the grid, false otherwise
+     */
+    public boolean controlAdjacencyFirstDie(int row, int column) {
+        if (row == WindowPattern.ROWS - 1 || row == 0) {
+            return true;
+        }
+        if (column == WindowPattern.COLUMNS - 1 || column == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Controls if in the adjacent cells there are dice with the same number or color
+     *
+     * @param grid
+     * @param die
+     * @param row
+     * @param column
+     * @return true if there isn't a dice with the same number and color nearby, false otherwise
+     */
+    public boolean controlAdjacency(Cell grid[][], Die die, int row, int column) {
+        try {
+            if (!controlCell(grid, row - 1, column, die)) {
+                return false;
+            }
+            if (!controlCell(grid, row, column - 1, die)) {
+                return false;
+            }
+            if (!controlCell(grid, row, column + 1, die)) {
+                return false;
+            }
+            if (!controlCell(grid, row + 1, column, die)) {
+                return false;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(String.format("%s (grid[%d][%d])"));
+        }
+
+        return true;
+    }
+
+    /**
+     * Controls if the number and the color of the die at the given coordinates are different from the one of the given die
+     *
+     * @param grid
+     * @param row
+     * @param column
+     * @param die
+     * @return true if it's different, false otherwise
+     */
+    public boolean controlCell(Cell grid[][], int row, int column, Die die) {
+        try {
+            if (!grid[row][column].isEmpty()) {
+                if (grid[row][column].getDie().getNumber() == die.getNumber()) {
+                    return false;
+                }
+                if (grid[row][column].getDie().getColor() == die.getColor()) {
+                    return false;
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    /**
+     * Check if there is a die in diagonal or adjacent to the given position
+     *
+     * @param grid
+     * @param row
+     * @param column
+     * @return true when is found a die nearby, false if there isn't any
+     */
+    public boolean controlCellAdjacency(Cell grid[][], int row, int column) {
+        try {
+            for (int i = row - 1; i <= row + 1; i++) {
+                for (int j = column - 1; j <= column + 1; j++) {
+                    if (!grid[i][j].isEmpty()) {
+                        return true;
+                    }
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    /**
+     * Controls if the cell at the given coordinates is empty
+     *
+     * @param grid
+     * @param row
+     * @param column
+     * @return true if it's empty, false otherwise
+     */
+    public boolean controlEmptyCell(Cell grid[][], int row, int column) {
+        if (grid[row][column].isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/it/polimi/ingsw/ConstraintControllerTest.java
@@ -0,0 +1,119 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstraintControllerTest {
+
+    @Test
+    void controlAdjacencyFirstDie() {
+        ConstraintController constraintController = new ConstraintController();
+        for (int i = 0; i < WindowPattern.ROWS; i++) {
+            assertTrue(constraintController.controlAdjacencyFirstDie(i, 0));
+            assertTrue(constraintController.controlAdjacencyFirstDie(i, 4));
+        }
+        for (int i = 0; i < WindowPattern.COLUMNS; i++) {
+            assertTrue(constraintController.controlAdjacencyFirstDie(0, i));
+            assertTrue(constraintController.controlAdjacencyFirstDie(3, i));
+        }
+        assertFalse(constraintController.controlAdjacencyFirstDie(2, 3));
+    }
+
+    @Test
+    void controlAdjacency() {
+        ConstraintController constraintController = new ConstraintController();
+        Cell[][] grid = new Cell[WindowPattern.ROWS][WindowPattern.COLUMNS];
+        int w = 0;
+        for (int i = 0; i < WindowPattern.ROWS; i++) {
+            for (int j = 0; j < WindowPattern.COLUMNS; j++) {
+                grid[i][j] = new Cell();
+                w++;
+            }
+        }
+        Die[] dice = new Die[8];
+        dice[0] = new Die(SagradaColor.PURPLE);
+        dice[0].setNumber(1);
+        grid[0][3].setDie(dice[0]);
+        dice[1] = new Die(SagradaColor.BLUE);
+        dice[1].setNumber(2);
+        grid[1][2].setDie(dice[1]);
+        dice[2] = new Die(SagradaColor.YELLOW);
+        dice[2].setNumber(4);
+        grid[1][3].setDie(dice[2]);
+        dice[3] = new Die(SagradaColor.GREEN);
+        dice[3].setNumber(3);
+        grid[1][4].setDie(dice[3]);
+        dice[4] = new Die(SagradaColor.YELLOW);
+        dice[4].setNumber(6);
+        grid[2][2].setDie(dice[4]);
+        dice[5] = new Die(SagradaColor.RED);
+        dice[5].setNumber(6);
+        grid[2][3].setDie(dice[5]);
+        dice[6] = new Die(SagradaColor.RED);
+        dice[6].setNumber(5);
+        grid[2][4].setDie(dice[6]);
+        dice[7] = new Die(SagradaColor.PURPLE);
+        dice[7].setNumber(6);
+        grid[3][4].setDie(dice[7]);
+        assertFalse(constraintController.controlAdjacency(grid, grid[2][4].getDie(), 2, 4));
+        assertFalse(constraintController.controlAdjacency(grid, grid[2][2].getDie(), 2, 2));
+        assertTrue(constraintController.controlAdjacency(grid, grid[1][3].getDie(), 1, 3));
+
+    }
+
+    @Test
+    void controlCell() {
+        ConstraintController constraintController = new ConstraintController();
+        Cell[][] grid = new Cell[WindowPattern.ROWS][WindowPattern.COLUMNS];
+        Die dieForGrid = new Die(SagradaColor.RED);
+        dieForGrid.setNumber(5);
+        grid[3][4] = new Cell();
+        grid[3][4].setDie(dieForGrid);
+        Die die = new Die(SagradaColor.GREEN);
+        die.setNumber(3);
+        Die die1 = new Die(SagradaColor.RED);
+        die1.setNumber(3);
+        Die die2 = new Die(SagradaColor.GREEN);
+        die2.setNumber(5);
+        Die die3 = new Die(SagradaColor.RED);
+        assertFalse(constraintController.controlCell(grid, 3, 4, die1)); // same color
+        assertFalse(constraintController.controlCell(grid, 3, 4, die2)); // same number
+        assertFalse(constraintController.controlCell(grid, 3, 4, dieForGrid)); // same number and color
+        assertTrue(constraintController.controlCell(grid, 3, 4, die)); // different number and color
+
+    }
+
+    @Test
+    void controlCellAdjacency() {
+        ConstraintController constraintController = new ConstraintController();
+        Cell[][] grid = new Cell[WindowPattern.ROWS][WindowPattern.COLUMNS];
+        int w = 0;
+        for (int i = 0; i < WindowPattern.ROWS; i++) {
+            for (int j = 0; j < WindowPattern.COLUMNS; j++) {
+                grid[i][j] = new Cell();
+                w++;
+            }
+        }
+        assertFalse(constraintController.controlCellAdjacency(grid, 3, 4));
+        Die die = new Die(SagradaColor.GREEN);
+        grid[0][0].setDie(die);
+        assertTrue(constraintController.controlCellAdjacency(grid, 1, 1));
+        grid[1][3].setDie(die);
+        assertTrue(constraintController.controlCellAdjacency(grid, 2, 3));
+        grid[2][2].setDie(die);
+        grid[3][1].setDie(die);
+        assertTrue(constraintController.controlCellAdjacency(grid, 3, 2));
+    }
+
+    @Test
+    void controlEmptyCell() {
+        ConstraintController constraintController = new ConstraintController();
+        Cell[][] grid = new Cell[WindowPattern.ROWS][WindowPattern.COLUMNS];
+        grid[3][4] = new Cell();
+        assertTrue(constraintController.controlEmptyCell(grid, 3, 4));
+        Die die = new Die(SagradaColor.GREEN);
+        grid[3][4].setDie(die);
+        assertFalse(constraintController.controlEmptyCell(grid, 3, 4));
+    }
+}
