27b4cfebbee470ac7853fd146470ce032c27c33a
8549 <15671077+8549@users.noreply.github.com>
2018-05-21 10:50:57 +0200

    Server can now run on custom port

--- src/main/java/it/polimi/ingsw/network/runServer.java
+++ src/main/java/it/polimi/ingsw/network/runServer.java
@@ -3,13 +3,16 @@ package it.polimi.ingsw.network;
 import java.rmi.Naming;
 
 public class runServer {
+    private static final int DEFAULT_PORT = 3131;
+
     public static void main(String[] args) {
+
         try {
             //System.setSecurityManager(new RMISecurityManager());
-            java.rmi.registry.LocateRegistry.createRegistry(1099);
+            java.rmi.registry.LocateRegistry.createRegistry(DEFAULT_PORT);
 
             ServerInterface server = new RMIServer();
-            Naming.rebind("rmi://127.0.0.1/sagrada", server);
+            Naming.rebind("rmi://127.0.0.1:" + DEFAULT_PORT + "/sagrada", server);
             System.out.println("[System] Server is ready.");
         }catch (Exception e) {
             System.out.println("Server failed: " + e);
