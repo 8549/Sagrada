cdc403e9cf54e1213ae4a3ebcfa0d88668e885e2
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2018-05-26 18:37:00 +0200

    updaate game manager

--- src/main/java/it/polimi/ingsw/GameManager.java
+++ src/main/java/it/polimi/ingsw/GameManager.java
@@ -1,59 +1,63 @@
 package it.polimi.ingsw;
 
+import com.google.gson.reflect.TypeToken;
 import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.network.server.ServerInterface;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class GameManager {
-    private static final int CARDS_PER_PLAYER = 2;
+    private static final int PATTERN_CARDS_PER_PLAYER = 2;
+    private ServerInterface server;
     private List<Player> players;
     private RoundTrack roundTrack;
     private ScoreTrack scoreTrack;
     private ObjCard[] publicObjectiveCards;
     private ToolCard[] toolCard;
     private List<Die> draftPool;
+    private DiceBag diceBag;
     private Player firstPlayer;
+    private Player currentPlayer;
     private int currentTurn;
     private int currentRound;
     public static final int ROUNDS = 10;
 
-    public GameManager() {
-        super();
-        players= new ArrayList<Player>();
+    public GameManager(ServerInterface server, List<Player> players) {
+        this.server = server;
+        this. players = players;
     }
 
     /**
-     * This met
-     * }
-     * hod performs the initial game setup following Sagrada's rules: places roundtrack, places scoretrack,
+     * This method performs the initial game setup following Sagrada's rules: places roundtrack, places scoretrack,
      * drafts 3 tool cards, drafts 3 public objective cards, selects randomly the first player
      */
     private void gameSetup() {
 
         //place roud track
         roundTrack = RoundTrack.getInstance();
+        roundTrack.getRoundCounter();
 
         //init scoretrack
         scoreTrack = ScoreTrack.getIstance();
 
         //place toolcard
-        CardsDeck toolDeck = new CardsDeck("", null); //TODO
+        /*CardsDeck toolDeck = new CardsDeck("", null); //TODO
         for (int i = 0; i < 3; i++) {
             toolCard[i] = (ToolCard) toolDeck.getRandomCard();
-        }
+        }*/
 
         //obj pub
-        CardsDeck objDeck = new CardsDeck("", null); //TODO
+        /*CardsDeck objDeck = new CardsDeck("", null); //TODO
         for (int j = 0; j < 3; j++) {
             publicObjectiveCards[j] = (ObjCard) objDeck.getRandomCard();
-        }
+        }*/
 
+        Collections.shuffle(players);
         //select first random
-
-        Random random = new Random();
-        firstPlayer = players.get(random.nextInt(players.size()));
+        firstPlayer = players.get(0);
 
     }
 
@@ -63,31 +67,28 @@ public class GameManager {
      * window pattern difficulty)
      */
     private void playerSetup() {
-        //creare il deck perchè disogna estrarre le carte una volta sola e subito rimuoverle
-        CardsDeck privateObjectiveCardsDeck = new CardsDeck("", null); // TODO
+        //create deck, extract one time only and immediately delete cards
+        //CardsDeck privateObjectiveCardsDeck = new CardsDeck("", null); // TODO
 
-        // creare il deck perchè disogna estrarre le carte una volta sola e subito rimuoverle
-        CardsDeck patternCardsDeck = new CardsDeck("", null); // TODO
+        //create deck, extract one time only and immediately delete cards
+        CardsDeck patternCardsDeck = new CardsDeck("PatternCards.json", new TypeToken<List<PatternCard>>(){}.getType());
 
         for (Player player : players) {
 
             //obj priv
-            player.setPrivateObjectiveCard((ObjCard) privateObjectiveCardsDeck.getRandomCard());
+            //player.setPrivateObjectiveCard((ObjCard) privateObjectiveCardsDeck.getRandomCard());
 
             //pattern card
             List<PatternCard> choices = new ArrayList<>();
-            for (int i = 0; i < CARDS_PER_PLAYER; i++) {
+            for (int i = 0; i < PATTERN_CARDS_PER_PLAYER; i++) {
                 choices.add((PatternCard) patternCardsDeck.getRandomCard());
             }
-            // set pattern card da player;
 
+            // set pattern card da player;
             //TODO WAIT FOR PLAYER CHOICE
 
             //token
             player.setInitialTokens();
-
-            //pattern slide
-            //TODO ANIMAZIONE VIEW
         }
 
     }
@@ -107,16 +108,31 @@ public class GameManager {
     }
 
 
-    public void addPlayer(Player player) {
-        for   (Player p : players){
-            if (p.getName().equals(player.getName())){
-                System.err.println("Players already in the game! ");
-                return;
-            }
+    public void setNextTurn() {
+        if (currentTurn == 1) {
+        } else {
+            setNextRound();
         }
-
-        players.add(player);
-        System.out.println("Player : " + player.getName() + " entered the game");
-        System.out.println("Now the players are " + players.toString());
     }
+
+    public void setNextRound() {
+        if (currentRound == 10) {
+            endGame();
+        } else {
+        }
+    }
+
+    public void endGame() {
+
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+
 }
