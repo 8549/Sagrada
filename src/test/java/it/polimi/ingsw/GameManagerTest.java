a0caf21f86fdb667aa8cccd3a61d71b14107262d
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2018-06-22 10:36:39 +0200

    Changed test in gameManager

--- src/test/java/it/polimi/ingsw/GameManagerTest.java
+++ src/test/java/it/polimi/ingsw/GameManagerTest.java
@@ -17,7 +17,8 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameManagerTest {
-
+    String[] args = new String[3];
+    MainServer mainServer = new MainServer(args);
     @Test
     void endGame() {
     }
@@ -25,8 +26,6 @@ class GameManagerTest {
 
     @Test
     void disconnectPlayer() {
-        String[] args = new String[3];
-        MainServer mainServer = new MainServer(args);
         List<Player> players = new ArrayList<>();
         Player marco = new Player("marco");
         players.add(marco);
@@ -34,25 +33,19 @@ class GameManagerTest {
         players.add(giulia);
         Player andrea = new Player("andrea");
         players.add(andrea);
-        Player francesca = new Player("francesca");
-        players.add(francesca);
         DiceBag diceBag = DiceBag.getInstance();
         List<Player> roundPlayers = new ArrayList<>();
         roundPlayers.add(marco);
         roundPlayers.add(giulia);
         roundPlayers.add(andrea);
-        roundPlayers.add(francesca);
         Round round = new Round(roundPlayers, 1);
         GameManager gameManager = new GameManager(mainServer, players);
-
         gameManager.disconnectPlayer(giulia);
-        assertEquals(6, round.getTurns().size());
+        assertEquals(PlayerStatus.PASSIVE, giulia.getStatus());
     }
 
     @Test
     void reconnectPlayer() {
-        String[] args = new String[3];
-        MainServer mainServer = new MainServer(args);
         List<Player> players = new ArrayList<>();
         Player marco = new Player("marco");
         players.add(marco);
@@ -60,47 +53,15 @@ class GameManagerTest {
         players.add(giulia);
         Player andrea = new Player("andrea");
         players.add(andrea);
-        Player francesca = new Player("francesca");
-        players.add(francesca);
         DiceBag diceBag = DiceBag.getInstance();
         List<Player> roundPlayers = new ArrayList<>();
         roundPlayers.add(marco);
         roundPlayers.add(giulia);
         roundPlayers.add(andrea);
-        roundPlayers.add(francesca);
         Round round = new Round(roundPlayers, 1);
         GameManager gameManager = new GameManager(mainServer, players);
-
         gameManager.disconnectPlayer(giulia);
-        assertEquals(6, round.getTurns().size());
-
-
-        gameManager.reconnectPlayer(francesca);
-        assertEquals(8, round.getTurns().size());
-    }
-
-    @Test
-    void gameLoop() {
-        String[] args = new String[3];
-        MainServer mainServer = new MainServer(args);
-        List<Player> players = new ArrayList<>();
-        Player marco = new Player("marco");
-        players.add(marco);
-        Player giulia = new Player("giulia");
-        players.add(giulia);
-        Player andrea = new Player("andrea");
-        players.add(andrea);
-        Player francesca = new Player("francesca");
-        players.add(francesca);
-        DiceBag diceBag = DiceBag.getInstance();
-        List<Player> roundPlayers = new ArrayList<>();
-        roundPlayers.add(marco);
-        roundPlayers.add(giulia);
-        roundPlayers.add(andrea);
-        roundPlayers.add(francesca);
-        Round round = new Round(roundPlayers, 1);
-        GameManager gameManager = new GameManager(mainServer, players);
-        gameManager.startRound();
-
+        gameManager.reconnectPlayer(giulia);
+        assertEquals(PlayerStatus.ACTIVE, giulia.getStatus());
     }
 }
