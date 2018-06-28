d860cbbb214d569be3816fe052b22d39c14d1712
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2018-06-28 12:05:03 +0200

    deleted to do methods for tool cards and added tool card handler

--- src/main/java/it/polimi/ingsw/GameManager.java
+++ src/main/java/it/polimi/ingsw/GameManager.java
@@ -303,38 +303,4 @@ public class GameManager {
         return round;
     }
 
-    public MainServer getServer() {
-        return server;
-    }
-
-    //methods for TOOL CARDS
-
-    public void chooseDieFromWindowPattern() {
-    }
-
-    public void chooseDieFromDraftPool() {
-    }
-
-    public void chooseDieFromRoundTrack() {
-    }
-
-    public void chooseIfDecrease() {
-    }
-
-    public void chooseIfPlaceDie() {
-    }
-
-    public void chooseToMoveOneDie() {
-        //if he chooses to move just one die everythingIsOk is set to false so the tool card won't keep performing effects
-    }
-
-    public void setValue() {
-    }
-
-    public void setOldCoordinates() {
-    }
-
-    public void setNewCoordinates() {
-    }
-
 }
---
+++ src/main/java/it/polimi/ingsw/ToolCardHandler.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw;
+
+public class ToolCardHandler {
+    public void chooseDieFromWindowPattern() {
+    }
+
+    public void chooseDieFromDraftPool() {
+    }
+
+    public void chooseDieFromRoundTrack() {
+    }
+
+    public void chooseIfDecrease() {
+    }
+
+    public void chooseIfPlaceDie() {
+    }
+
+    public void chooseToMoveOneDie() {
+        //if he chooses to move just one die everythingIsOk is set to false so the tool card won't keep performing effects
+    }
+
+    public void setValue() {
+    }
+
+    public void setOldCoordinates() {
+    }
+
+    public void setNewCoordinates() {
+    }
+
+}
