c12c3626f9a6084284989e403f92ec57f79f06cc
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2018-07-03 19:57:54 +0200

    modified tool cards

--- src/test/java/it/polimi/ingsw/model/ToolCardTest.java
+++ src/test/java/it/polimi/ingsw/model/ToolCardTest.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.model;
 import com.google.gson.reflect.TypeToken;
 import it.polimi.ingsw.model.effect.*;
 import it.polimi.ingsw.serialization.GsonSingleton;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +11,15 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToolCardTest {
-
-   @org.junit.jupiter.api.Test
-    void verifyToolCard(){
+    @Test
+    void testVerifyToolCard(){
         String name = "Tap Wheel";
         int id= 12;
         String when= "always";
         List<Effect> features = new ArrayList<>();
         Effect effect= new ChooseToMoveOneOrTwoDice("chooseToMoveOneOrTwoDice");
-        Effect effect1 = new ChooseDieFromRoundTrackEffect("chooseDieFromRoundTrack");
+        Effect effect1;
+        effect1 = new ChooseDieFromRoundTrackEffect("chooseDieFromRoundTrack");
         Effect effect2 = new ChooseDieFromWindowPattern("chooseDieFromWindowPattern");
         Effect effect3 = new MoveDieWithSameColorAsDieFromRoundTrackEffect("moveDieWithSameColorAsDieFromRoundTrack");
         features.add(effect1);
@@ -34,5 +34,133 @@ class ToolCardTest {
         Card expectedCard = deck.getByName(name);
         Effect t = GsonSingleton.getInstance().fromJson("moveDieWithoutColorConstraint", Effect.class);
         assertEquals(expectedCard, tapWheel);
+    }
+
+    @Test
+    void initReferences() {
+    }
+
+    @Test
+    void testIsUsed() {
+        ToolCard toolCard= new ToolCard();
+        assertFalse(toolCard.isUsed());
+    }
+
+
+    @Test
+    void testGetCost() {
+        ToolCard toolCard= new ToolCard();
+        assertEquals(1, toolCard.getCost());
+    }
+
+
+    @Test
+    void testAddTokens() {
+        ToolCard toolCard= new ToolCard();
+        toolCard.addTokens();
+        toolCard.addTokens();
+        assertEquals(2, toolCard.getTokens());
+    }
+
+    @Test
+    void useTools() {
+    }
+
+    @Test
+    void performEffect() {
+    }
+
+    @Test
+    void checkHasNextEffect() {
+    }
+
+    @Test
+    void testGetTokens() {
+        ToolCard toolCard= new ToolCard();
+        toolCard.addTokens();
+        toolCard.addTokens();
+        assertEquals(2, toolCard.getTokens());
+    }
+
+    @Test
+    void addDieToDiceBag() {
+        ToolCard toolCard= new ToolCard();
+        Board board =new Board();
+        board.setDiceBag();
+        board.getDiceBag().draftDie();
+        Die die =board.getDiceBag().draftDie();
+        toolCard.addDieToDiceBag(die);
+        //assertTrue(j);
+    }
+
+    @Test
+    void processMoveWithoutConstraints() {
+    }
+
+    @Test
+    void getDieFromDicePool() {
+    }
+
+    @Test
+    void chooseDieFromWindowPattern() {
+    }
+
+    @Test
+    void chooseDieFromDraftPool() {
+    }
+
+    @Test
+    void chooseDieFromRoundTrack() {
+    }
+
+    @Test
+    void chooseIfDecrease() {
+    }
+
+    @Test
+    void chooseIfPlaceDie() {
+    }
+
+    @Test
+    void chooseToMoveOneDie() {
+    }
+
+    @Test
+    void setValue() {
+    }
+
+    @Test
+    void setOldCoordinates() {
+    }
+
+    @Test
+    void setNewCoordinates() {
+    }
+
+    @Test
+    void completeProcessMove() {
+    }
+
+    @Test
+    void completeChooseDieFromWindowPattern() {
+    }
+
+    @Test
+    void setResponse() {
+    }
+
+    @Test
+    void completeChooseValue() {
+        ToolCard toolCard= new ToolCard();
+        toolCard.completeChooseValue(2);
+        //assertTrue(toolCard.isEverythingOk());
+    }
+
+    @Test
+    void setParameters() {
+    }
+
+    @Test
+    void getToolCardHandler() {
     }
 }
