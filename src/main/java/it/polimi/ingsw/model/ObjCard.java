99508efaa126a5cd4a2a1ed35db7b090555d5fd7
8549 <15671077+8549@users.noreply.github.com>
2018-07-03 16:31:22 +0200

    split enum from class

--- src/main/java/it/polimi/ingsw/model/ObjCard.java
+++ src/main/java/it/polimi/ingsw/model/ObjCard.java
@@ -4,19 +4,12 @@ import java.io.Serializable;
 
 public abstract class ObjCard implements Card, Serializable{
     String name;
-    private int points;
     ObjectiveCardType type;
     ObjectiveCardWhere where;
     PatternConstraint[] rules;
     Prize prize;
 
     public abstract int checkObjective(Cell[][] grid);
-
-    class Prize implements Serializable {
-        PrizeType type;
-        int value;
-
-    }
 
     public String getName(){
         return this.name;
---
+++ src/main/java/it/polimi/ingsw/model/Prize.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.model;
+
+import java.io.Serializable;
+
+class Prize implements Serializable {
+    PrizeType type;
+    int value;
+}
