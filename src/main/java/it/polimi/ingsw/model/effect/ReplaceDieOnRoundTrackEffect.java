4c6d3e951a8e76177d8a2e4186aefd3b05cd1b83
martinabellinipoli <37836894+martinabellinipoli@users.noreply.github.com>
2018-07-07 14:17:38 +0200

    fixed effect

--- src/main/java/it/polimi/ingsw/model/effect/ReplaceDieOnRoundTrackEffect.java
+++ src/main/java/it/polimi/ingsw/model/effect/ReplaceDieOnRoundTrackEffect.java
@@ -14,13 +14,15 @@ public class ReplaceDieOnRoundTrackEffect extends Effect {
         Die die = (Die) args[0];
         int turn = (int) args[1];
         int numberOfDie = (int) args[2];
-        if (toolCard.getBoard().getRoundTrack().getDieAt(turn, numberOfDie) == null) {
-            toolCard.setResponse(false);
-        } else {
-            toolCard.getBoard().getDraftPool().add(toolCard.getBoard().getRoundTrack().replaceDie(die, turn, numberOfDie));
+        Die die1 = toolCard.getBoard().getRoundTrack().replaceDie(die, turn, numberOfDie);
+        if(die1!=null) {
+            toolCard.getBoard().getDraftPool().add(die1);
+            toolCard.getBoard().getDraftPool().remove(die);
             toolCard.getToolCardHandler().updateDraftPool(toolCard.getBoard().getDraftPool());
             toolCard.getToolCardHandler().updateRoundTrack(die, numberOfDie, turn);
             toolCard.setResponse(true);
+        } else{
+            toolCard.setResponse(false);
         }
     }
 }
